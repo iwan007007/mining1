5007e2a63f637a79fdd9f29b53e83bd93a737e32
iwan007007 <118505746+iwan007007@users.noreply.github.com>
2022-11-18 14:42:14 +0700

    Create ming1.sh

---
+++ ming1.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u TRfoyJQuR1CDVaQGmyeuP8fA13qc6ACcaC -p c=TRX -t 2
